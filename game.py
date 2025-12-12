3372952daecd9aff2c1d5aec0f472fb7377a86dc
Max Smirnov <makssabdal@gmail.com>
2025-12-12 18:36:01 +0300

    add if __name__ == '__main__'

--- game.py
+++ game.py
@@ -1,8 +1,14 @@
 from gameparts import Board
 
-game = Board()
-game.display()
-game.make_move(1, 1, 'X')
-game.make_move(2, 1, '0')
-print('Ход сделан!')
-game.display()
+
+def main():
+    game = Board()
+    game.display()
+    game.make_move(1, 1, 'X')
+    game.make_move(2, 1, '0')
+    print('Ход сделан!')
+    game.display()
+
+
+if __name__ == '__main__':
+    main()
