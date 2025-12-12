87ad1df2b0d5321903ee6885beec5f804283c844
Max Smirnov <makssabdal@gmail.com>
2025-12-12 15:59:11 +0300

    create module parts

--- game.py
+++ game.py
@@ -1,19 +1,8 @@
-class Board:
-
-    def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
-
-    def make_move(self, row, col, player):
-        self.board[row][col] = player
-
-    def display(self):
-        for row in self.board:
-            print('|'.join(row))
-            print('-' * 5)
-
+from parts import Board
 
 game = Board()
 game.display()
 game.make_move(1, 1, 'X')
+game.make_move(2, 1, '0')
 print('Ход сделан!')
 game.display()
