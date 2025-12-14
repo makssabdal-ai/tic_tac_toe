e530826590027db51dd19cd8d20b2da1995ad65a
Max Smirnov <makssabdal@gmail.com>
2025-12-14 16:27:41 +0300

    add save_result; change game.display

--- game.py
+++ game.py
@@ -1,59 +1,77 @@
+# game.py
+
 from gameparts import Board
-from gameparts.exceptions import FieldIndexError
+from gameparts.exceptions import CellOccupiedError,  FieldIndexError
 
 
 def main():
     game = Board()
+    # Первыми ходят крестики.
+    current_player = 'X'
+    # Это флаговая переменная. По умолчанию игра запущена и продолжается.
+    running = True
     game.display()
 
-    # Запускается бесконечный цикл.
-    while True:
-        # В этом блоке содержатся операции, которые могут вызвать исключение.
-        try:
-            # Пользователь вводит значение номера строки.
-            row = int(input('Введите номер строки: '))
-            # Если введённое число меньше 0 или больше
-            # или равно game.field_size...
-            if row < 0 or row >= game.field_size:
-                # ...выбрасывается собственное исключение FieldIndexError.
-                raise FieldIndexError
-            column = int(input('Введите номер столбца: '))
-            # Если введённое число меньше 0 или больше
-            # или равно game.field_size...
-            if column < 0 or column >= game.field_size:
-                # ...выбрасывается собственное исключение FieldIndexError.
-                raise FieldIndexError
-        # Если возникает исключение FieldIndexError...
-        except FieldIndexError:
-            # ...выводятся сообщения...
-            print(
-                'Значение должно быть неотрицательным и меньше '
-                f'{game.field_size}.'
-            )
-            print('Пожалуйста, введите значения для строки и столбца заново.')
-            # ...и цикл начинает свою работу сначала,
-            # предоставляя пользователю ещё одну попытку ввести данные.
-            continue
-        except ValueError:
-            print(
-                 'Буквы вводить нельзя. Только числа.'
-                 )
-            print(
-                 'Пожалуйста, введите значения для строки и столбца заново.'
-                 )
-            continue
-        except Exception as e:
-            print(f'Возникла ошибка: {e}')
-        # Если в блоке try исключения не возникло...
-        else:
-            # ...значит, введённые значения прошли все проверки
-            # и могут быть использованы в дальнейшем.
-            # Цикл прерывается.
-            break
+    # Тут запускается основной цикл игры.
+    while running:
 
-    game.make_move(row, column, 'X')
-    print('Ход сделан!')
-    game.display()
+        print(f'Ход делают {current_player}')
+
+        # Запускается бесконечный цикл.
+        while True:
+            try:
+                row = int(input('Введите номер строки: '))
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца: '))
+                if column < 0 or column >= game.field_size:
+                    raise FieldIndexError
+                if game.board[row][column] != ' ':
+                    # Вот тут выбрасывается новое исключение.
+                    raise CellOccupiedError
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}.'
+                )
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except ValueError:
+                print('Буквы вводить нельзя. Только числа.')
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except CellOccupiedError:
+                print('Ячейка занята')
+                print('Введите другие координаты.')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
+            else:
+                break
+
+        # Теперь для установки значения на поле само значение берётся
+        # из переменной current_player.
+        game.make_move(row, column, current_player)
+        game.display()
+        if game.check_win(current_player):
+            result = f'Победили {current_player}!'
+            print(result)
+            save_result(result + '\n')
+            running = False
+        elif game.is_board_full():
+            result = 'Ничья!'
+            print(result)
+            save_result(result + '\n')
+            running = False
+        # Тернарный оператор, через который реализована смена игроков.
+        # Если current_player равен X, то новым значением будет O,
+        # иначе — новым значением будет X.
+        current_player = 'O' if current_player == 'X' else 'X'
+
+
+def save_result(result):
+    with open('tic_tac_toe/results.txt', 'a', encoding='utf-8') as file:
+        file.write(result + '\n')
 
 
 if __name__ == '__main__':
